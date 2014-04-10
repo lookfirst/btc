7026ea83f68a9089659a19f4e4db7be77e706d24
Jon Stevens <latchkey@gmail.com>
2014-04-10 16:02:09 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,5 +7,5 @@ else
 	git pull origin master
 fi
 
-CFLAGS="-O2 -Wall" ./configure --enable-hashfast && make
+CFLAGS="-O2 -Wall" ./configure --enable-hashfast && make install
 sudo cp 01-cgminer.rules /etc/udev/rules.d/
