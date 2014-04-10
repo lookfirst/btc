6eca40ea3e5234596fe33e6c1a1643b7c0927d0d
Jon Stevens <latchkey@gmail.com>
2014-04-10 16:27:59 -0700

    Update cgminer-start.sh

--- etc/init.d/cgminer-start.sh
+++ etc/init.d/cgminer-start.sh
@@ -4,7 +4,7 @@ sleep 5
 while [ 1 ]; do
 	echo "starting screen/cgminer"
 	sleep 2
-	/usr/bin/screen -dmS cgminer /opt/cgminer/cgminer -c /opt/btc/conf/cgminer.conf
+	/usr/bin/screen -dmS cgminer /usr/local/bin/cgminer -c /opt/btc/conf/cgminer.conf
 	CHILD="$!"
 	trap "kill $CHILD; exit" exit INT TERM
 	wait
