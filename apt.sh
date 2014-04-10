dce103405345db7482c8cffc34d6456c433a7337
Jon Stevens <latchkey@gmail.com>
2014-04-10 13:05:09 -0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -4,4 +4,7 @@ aptitude purge -y wolfram-engine python3-pifacedigital-scratch-handler omxplayer
 aptitude update -y
 aptitude safe-upgrade -y
 aptitude install -y screen htop build-essential autoconf automake libtool pkg-config libcurl3-dev libudev-dev libcurl4-openssl-dev libncurses5-dev
+
+update-rc.d -f triggerhappy remove
+
 rpi-update
