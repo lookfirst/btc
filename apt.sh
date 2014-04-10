8e3c8a897f17b5a00ce9313ca296859ac308054b
Jon Stevens <latchkey@gmail.com>
2014-04-10 16:47:48 -0700

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -3,7 +3,7 @@
 aptitude purge -y wolfram-engine python3-pifacedigital-scratch-handler omxplayer pulseaudio
 aptitude update -y
 aptitude safe-upgrade -y
-aptitude install -y screen htop build-essential autoconf automake libtool pkg-config libcurl3-dev libudev-dev libcurl4-openssl-dev libncurses5-dev
+aptitude install -y gawk screen htop build-essential autoconf automake libtool pkg-config libcurl3-dev libudev-dev libcurl4-openssl-dev libncurses5-dev
 
 update-rc.d -f triggerhappy remove
 
