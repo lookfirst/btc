bc0ef6e7bd717ab0e7610fe688bf69cd26232722
Jon Stevens <latchkey@gmail.com>
2014-04-10 19:25:03 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,8 @@
 cp etc/init.d/cgminer /etc/init.d
 update-rc.d cgminer defaults
 
-cp conf/cgminer-base.conf conf/cgminer.conf
+if [ -f "conf/cgminer.conf" ]; then
+	echo "Not overwriting your cgminer.conf! Check the base file for relevant changes."
+else
+	cp conf/cgminer-base.conf conf/cgminer.conf
+fi
